401fb0bd56ae40553ccedcedf04570e0f3688fe1
Jorge Palma <jorgepalmaconceicao@gmail.com>
2025-01-19 20:35:51 -0300

    Adicionado escopo da função para piscar todos os leds sinalizando que acabou a animação

--- include/pisca_led.h
+++ include/pisca_led.h
@@ -4,4 +4,5 @@
 #include "pico/bootrom.h"  // Biblioteca para acessar o modo bootloader
 
 void blink_led(uint8_t pin, uint32_t duration);
+void piscar_todos_os_leds_finalizando(uint8_t LED_R_PIN, uint8_t LED_G_PIN, uint8_t LED_B_PIN);
 void run_pisca_led_diferentes_frequencias(uint8_t LED_R_PIN, uint8_t LED_G_PIN, uint8_t LED_B_PIN);
