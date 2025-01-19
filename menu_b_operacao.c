acc52c674a147c6111d6356c650952f979fe54df
Eu, de paula <gfpaula.cic@uesc.br>
2025-01-19 04:36:00 -0300

    mais instancias das funções do modo B

--- menu_b_operacao.c
+++ menu_b_operacao.c
@@ -1,1 +1,14 @@
 #include "menu_b_operacao.h"
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "pico/bootrom.h"  // Biblioteca para acessar o modo bootloader
+
+
+void operacao_inicial()
+{
+    int tecla = read_keypad();
+}
+void executa_leds(int *linha_comando_led);
+void executa_buzzer(int *linha_comando_buzz);
+void interrupcaoBotao(uint gpio, uint32_t events);
