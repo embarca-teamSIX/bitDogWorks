404d369b0e8da4efbe9d916e6cb799fbab57b070
Jorge Palma <jorgepalmaconceicao@gmail.com>
2025-01-19 20:22:41 -0300

    Incluindo função de piscar o led

--- pisca_led.c
+++ pisca_led.c
@@ -7,6 +7,13 @@
 int intervals[] = {10, 1000, 750, 500, 250};
 const int num_intervals = sizeof(intervals) / sizeof(intervals[0]);
 
+void blink_led(uint8_t pin, uint32_t duration) {
+    gpio_put(pin, 1);         // Liga o LED
+    sleep_ms(duration);       // Aguarda pelo tempo especificado
+    gpio_put(pin, 0);         // Desliga o LED
+    sleep_ms(duration);       // Tempo para desligado
+}
+
 void run_pisca_led_diferentes_frequencias(uint8_t LED_R_PIN, uint8_t LED_G_PIN, uint8_t LED_B_PIN)
 {
     // Para cada frequência definida
