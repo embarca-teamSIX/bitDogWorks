00e07ac18f8ef0d5e0e5741e895531af102211af
Eu, de paula <gfpaula.cic@uesc.br>
2025-01-19 01:51:37 -0300

    instancias de funções para operacao com led e buzz

---
+++ include/menu_b_operacao.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "pico/bootrom.h"  // Biblioteca para acessar o modo bootloader
+
+void operacao_inicial();
+void executa_leds(int *linha_comando_led);
+void executa_buzzer(int *linha_comando_buzz);
+void interrupcaoBotao(uint gpio, uint32_t events);
